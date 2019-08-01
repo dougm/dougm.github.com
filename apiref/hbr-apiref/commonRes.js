14d9b671f68b7eacfb844ec9318b045c5002325b
Doug MacEachern <dougm@vmware.com>
2019-08-01 10:39:09 -0700

    add apiref from vimbase build 12317751 (vSphere 6.7u2)

---
+++ apiref/hbr-apiref/commonRes.js
@@ -0,0 +1,2 @@
+var ID_Copyright = "&copy; 2009 VMware, Inc. All rights reserved.";
+var ID_VersionInformation = "Revision 20090507 Version 4.0";
---
+++ apiref/pbm-apiref/commonRes.js
@@ -0,0 +1,3 @@
+var ID_Copyright = "&copy; 2012-2018 VMware, Inc. All rights reserved.";
+var ID_VersionInformation = "Revision 4-OCT-2018 &nbsp;|&nbsp;VMware Storage Policy API &nbsp;|&nbsp; Version 6.7 Update 1";
+
